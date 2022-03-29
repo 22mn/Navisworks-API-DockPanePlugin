048c675133b27045ded892757f3bf2d1922efc7e
Min Naung <p.jean90@gmail.com>
2022-03-29 21:31:47 +0800

    add files via upload

---
+++ MainClass.cs
@@ -0,0 +1,39 @@
+﻿using Autodesk.Navisworks.Api.Plugins;
+using System.Windows.Forms.Integration;
+using System.Windows.Forms;
+
+namespace WPFDockablePane
+{
+    // assign name, devId 
+    [Plugin("WPFDockablePane", "TwentyTwo", DisplayName ="TwentyTwo {DockablePane Sample}",
+        ToolTip="A tutorial project by TwentyTwo.")]
+    // set preferred size 
+    [DockPanePlugin(350, 500, FixedSize =false)]
+
+    public class MainClass : DockPanePlugin 
+    {
+        // Called to tell the plugin to create it's pane.
+        // Should be overriden in conjunction with DestroyControlPane.
+        public override Control CreateControlPane()
+        {
+            // WindowForm contl to host WPF elem
+            ElementHost elemHost = new ElementHost
+            {
+                // assign wpfcontrol
+                AutoSize = true,
+                Child = new WPFControl()
+            };
+            // create control
+            elemHost.CreateControl();
+            
+            return elemHost;
+        }
+        // Called to tell the plugin to destroy it's pane.
+        // Should be overriden in conjunction with CreateControlPane.
+        public override void DestroyControlPane(Control pane)
+        {
+            // releases all resources /dispose
+            pane.Dispose();
+        }
+    }
+}
---
+++ WPFControl.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows.Controls;
+using System.Reflection;
+
+namespace WPFDockablePane
+{
+    /// <summary>
+    /// Interaction logic for WPFControl.xaml
+    /// </summary>
+    public partial class WPFControl : UserControl
+    {
+        public WPFControl()
+        {
+            // initialize
+            InitializeComponent();
+
+            // navigated event register 
+            // to hide unwanted JS Script pop-ups 
+            wb.Navigated += (a, b) => { HideScriptErrors(wb, true); };
+        }
+        
+        // callback method
+        public void HideScriptErrors(WebBrowser wb, bool hide)
+        {
+            // get webbrowser's a private field / _axIWebBrowser2
+            // field search included instance & non-public members /private field
+            FieldInfo fiComWebBrowser = typeof(WebBrowser).GetField("_axIWebBrowser2", 
+                BindingFlags.Instance | BindingFlags.NonPublic);
+            if (fiComWebBrowser == null) return;
+
+            // get COM webbrowser obj
+            var objComWebBrowser = fiComWebBrowser.GetValue(wb);
+            if (objComWebBrowser == null)
+            {
+                // in case too early
+                wb.Loaded += (o, s) => HideScriptErrors(wb, hide); 
+                return;
+            }
+            // invokes the specified member, using the specified binding constraints 
+            // and matching the specified argument list
+            objComWebBrowser.GetType().InvokeMember("Silent", BindingFlags.SetProperty, 
+                null, objComWebBrowser, new object[] { hide });
+        }
+    }
+}
